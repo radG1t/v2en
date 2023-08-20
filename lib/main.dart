68633f24ee57ba45ba0ce52d059b919a80481749
Radin <radinerfanfar@gmail.com>
2023-08-20 14:02:07 +0330

    working on menu-corner right

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'v2en demo',
       theme: ThemeData(
         primaryColor: Colors.white,
-        colorScheme: ColorScheme.dark(background: Colors.white),
+        colorScheme: const ColorScheme.dark(background: Colors.white),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -34,83 +34,86 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      /*  appBar: AppBar(
+    return SafeArea(
+      child: Scaffold(
         backgroundColor: Colors.black,
-        title: const Center(
-          child: Text(
-            'v2en login',
-            style: TextStyle(color: Colors.white),
+        /*  appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Center(
+            child: Text(
+              'v2en login',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-        ),
-      ), */
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(22.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const SizedBox(
-                height: 140,
-              ),
-              ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(70)),
-                child: Image.asset(
-                  'assets/zenLogo.jpg',
-                  fit: BoxFit.fill,
-                  width: 200,
-                  height: 200,
+        ), */
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(22.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(
+                  height: 140,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'V2en Login',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-              const SizedBox(
-                height: 85,
-              ),
-              const SizedBox(
-                width: 200,
-                child: TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Username',
-                      labelStyle: TextStyle(color: Colors.white),
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white))),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const SizedBox(
-                width: 200,
-                child: TextField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    labelText: 'Password',
-                    //  labelStyle: TextStyle(color: Colors.white),
+                ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(70)),
+                  child: Image.asset(
+                    'assets/zenLogo.jpg',
+                    fit: BoxFit.fill,
+                    width: 200,
+                    height: 200,
                   ),
-                  obscureText: true,
                 ),
-              ),
-              const SizedBox(
-                height: 35,
-              ),
-              ElevatedButton(
-                  style: const ButtonStyle(
-                      fixedSize: MaterialStatePropertyAll(Size(20, 20))),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const MainPage()),
-                    );
-                  },
-                  child: const Icon(CupertinoIcons.check_mark))
-            ],
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'V2en Login',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 85,
+                ),
+                const SizedBox(
+                  width: 200,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: 'Username',
+                        labelStyle: TextStyle(color: Colors.white),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white))),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const SizedBox(
+                  width: 200,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      labelText: 'Password',
+                      //  labelStyle: TextStyle(color: Colors.white),
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+                const SizedBox(
+                  height: 35,
+                ),
+                ElevatedButton(
+                    style: const ButtonStyle(
+                        fixedSize: MaterialStatePropertyAll(Size(20, 20))),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const MainPage()),
+                      );
+                    },
+                    child: const Icon(CupertinoIcons.check_mark))
+              ],
+            ),
           ),
         ),
       ),
--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -11,11 +11,54 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.teal[900],
-        title: const Text('Server list'),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          // elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.teal[900],
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('Server list'),
+              IconButton(
+                icon: const Icon(CupertinoIcons.settings),
+                onPressed: () {
+                  const DropdownMenu(
+                    label: Text('data'),
+                    hintText: 'test',
+                    dropdownMenuEntries: [],
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+            color: Colors.black45,
+            height: 60,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('not connected'),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Container(
+                    color: Colors.teal,
+                    child: IconButton(
+                      color: Colors.white,
+                      onPressed: () {},
+                      icon: const Icon(
+                        size: 20,
+                        CupertinoIcons.antenna_radiowaves_left_right,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
